0c254591d918b769e51f6b636334e28b83824157
wairimu.Kanene <mercykanene254@gmail.com>
2022-04-20 15:14:30 +0300

    commit8-added the routing on my profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 const routes: Routes = [
+  {path:"", component:NavBarComponent},
   {path:"profile", component:HomePageComponent}
 ];
 
