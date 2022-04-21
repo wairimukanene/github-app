5619ec940a83498ec08e158520a7bbaefbf51971
wairimu.Kanene <mercykanene254@gmail.com>
2022-04-21 10:50:43 +0300

    Auto-generated commit [ci skip]

--- src/app/repository.ts
+++
@@ -1,3 +0,0 @@
-export class Repository {
-  constructor(public name: any, public full_name: any, public description: any, public updated_at:Date, public html_url: any, public clone_url: any, public language: any, public created_at:Date){}
-}
--- src/app/user.ts
+++
@@ -1,4 +0,0 @@
-export class User {
-  constructor(public avatar_url:any, public login:any, public bio:any,public public_repos:any, public created_at:any
-){}
-}
